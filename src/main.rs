c50f1ad6ae40c517a33f8bbb271155377202bf8a
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2025-01-17 18:35:15 -0500

    Load timesheet

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
+use chrono::{DateTime, Utc};
 use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
-use std::{
-    collections::HashMap,
-    fs::File,
-    io::{Read, Write},
-    path::Path,
-    str::FromStr,
-};
+use std::{collections::HashMap, fs::File, io::Write, path::Path};
 
 /// Command usage should look like this
 /// timesheet clock midas (for clock in if closed)
@@ -25,6 +20,18 @@ struct Project {
 #[derive(Serialize, Deserialize, Debug)]
 struct IndexFile {
     projects: Vec<Project>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TimesheetEntry {
+    time_in: String,
+    time_out: String,
+    message: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Timesheet {
+    entries: Vec<TimesheetEntry>,
 }
 
 #[derive(Subcommand)]
@@ -55,9 +62,13 @@ fn main() {
     let args = Args::parse();
     let command = &args.command;
 
-    let index_map = load_project_index(Path::new("/home/gabriel/timesheets/index.json"));
+    let index_map: HashMap<String, String> =
+        load_project_index(Path::new("/home/gabriel/timesheets/index.json")).unwrap();
 
-    println!("{:?}", index_map);
+    let timesheet_vec: Vec<TimesheetEntry> =
+        load_project(Path::new(index_map.get("midas").expect("Ahhhh")));
+
+    println!("{:?}", timesheet_vec);
 
     match &args.command {
         Subcommands::Clock { project_name } => {}
@@ -79,15 +90,16 @@ fn load_project_index(path: &Path) -> Result<HashMap<String, String>, Box<dyn st
     let mut project_map: HashMap<String, String> = HashMap::new();
 
     // If index path file does not exist, create it.
-    if !&path
+    if !path
         .try_exists()
         .expect("Index path is indeterminate. You may not have read permission.")
     {
-        eprint!(
+        eprintln!(
             "Project index path not found. Creating index file at {}",
-            Some(&path).unwrap().display()
+            path.display()
         );
 
+        // Initialize JSON structure with no contents
         let initial_index = IndexFile {
             projects: Vec::new(),
         };
@@ -96,12 +108,15 @@ fn load_project_index(path: &Path) -> Result<HashMap<String, String>, Box<dyn st
 
         let mut file =
             File::create(path).expect("Creating project index path failed. Path is invalid.");
+
+        // Write the empty formatted JSON
         file.write_all(initial_json.as_bytes())
             .expect("Failed to write initial JSON to file.");
     }
 
     // Then read the index file, it will be empty if it was not found in the previous section
-    let data = std::fs::read_to_string(&path).expect("Could not load index path.");
+    let data = std::fs::read_to_string(path).expect("Could not load index path.");
+
     let index: IndexFile = serde_json::from_str(&data).expect("Could not read index file JSON.");
 
     for project in index.projects {
@@ -111,8 +126,46 @@ fn load_project_index(path: &Path) -> Result<HashMap<String, String>, Box<dyn st
     Ok(project_map)
 }
 
-fn load_project(path: &Path) {
-    todo!();
+fn load_project(path: &Path) -> Vec<TimesheetEntry> {
+    // If timesheet path file does not exist, create it.
+    if !path
+        .try_exists()
+        .expect("Index path is indeterminate. You may not have read permission.")
+    {
+        eprintln!(
+            "Project index path not found. Creating index file at {}",
+            path.display()
+        );
+
+        // Initialize JSON structure with no contents
+        let initial_timesheet = Timesheet {
+            entries: Vec::new(),
+        };
+
+        let initial_json = serde_json::to_string_pretty(&initial_timesheet)
+            .expect("Failed to serialize initial index structure.");
+
+        let mut file =
+            File::create(path).expect("Creating project index path failed. Path is invalid.");
+
+        // Write the empty formatted JSON
+        file.write_all(initial_json.as_bytes())
+            .expect("Failed to write initial JSON to file.");
+    }
+
+    // Load the data, it will be empty if it was just created above
+    let data = std::fs::read_to_string(&path).expect("Could not load timesheet path.");
+
+    let timesheet: Timesheet =
+        serde_json::from_str(&data).expect("Could not read timesheet file JSON.");
+
+    let mut entries: Vec<TimesheetEntry> = vec![];
+
+    for entry in timesheet.entries {
+        entries.push(entry);
+    }
+
+    entries
 }
 
 fn clock_in(project: &Project) {
